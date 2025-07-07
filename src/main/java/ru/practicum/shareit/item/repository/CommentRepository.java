e87e6c6a9d321299f296c0b0c967bc2f9f6db7c9
dskryga <dskryga@gmail.com>
2025-07-07 17:27:45 +0300

    codestyle changes

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -8,8 +8,9 @@ import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     Collection<Comment> findAllByItemIdOrderByCreatedTimeAsc(Long itemId);
+
     Collection<Comment> findAllByItemIdIn(List<Long> itemIds);
 
 }
